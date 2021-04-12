bd09621582848317a754eba87e782083687e503e
aftarfadilah <aftar.fadilah99@gmail.com>
2021-04-12 21:03:42 +0800

    feat(ui): add tab bar and tab view placeholders for menu

--- lib/MenuState.dart
+++ lib/MenuState.dart
@@ -2,33 +2,74 @@ import 'package:flutter/material.dart';
 
 class MenuState extends StatelessWidget {
   final String title;
-
   MenuState(this.title);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 0,
-        title: Text(
-          "Meja $title",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontFamily: 'Montserrat',
-            fontWeight: FontWeight.w900,
-            color: Colors.black87,
-          ),
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: Text(
+                "Meja $title",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w900,
+                  color: Colors.black87,
+                ),
+              ),
+              backgroundColor: Colors.white,
+              elevation: 2,
+              iconTheme: IconThemeData(
+                  color: Colors.black87
+              ),
+              bottom: TabBar(
+                  labelColor: Colors.green,
+                  unselectedLabelColor: Colors.black87,
+                tabs: [
+                  Tab(
+                    text: 'Lauk',
+                  ),
+                  Tab(
+                    text: 'Sayuran',
+                  ),
+                  Tab(
+                    text: 'Minuman',
+                  )
+                ],
+                indicator: UnderlineTabIndicator(
+                  borderSide: BorderSide(
+                    color: Colors.green,
+                    width: 2,
+                  )
+                )
+              ),
+            ),
+            body: TabBarView( // TODO: Add Menu Placeholder
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text("Lauk untuk Meja $title")
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text("Sayuran untuk Meja $title")
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text("Minuman untuk Meja $title")
+                  ],
+                ),
+              ],
+            )
         ),
-        backgroundColor: Colors.white,
-      ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          // TODO: Add Menu Placeholder
-          Text("Menu untuk Meja $title");
-        ],
-      ),
     );
   }
 }
